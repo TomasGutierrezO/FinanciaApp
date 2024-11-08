6a2f7b37034a5d26f67b7576d3bfc99e2d64ae6b
Tomas <tomasgo2004@gmail.com>
2024-11-08 17:39:36 -0500

    Changes in Views, Add: test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 dependencies {
@@ -49,11 +53,19 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation ("androidx.test:core:1.4.0")
+    testImplementation ("com.google.truth:truth:1.4.2")
+    testImplementation ("org.robolectric:robolectric:4.12.2")
+    testImplementation ("org.mockito:mockito-core:5.12.0")
+    testImplementation ("org.mockito:mockito-inline:5.2.0") // For mocking final classes and methods
+    testImplementation ("junit:junit:4.13.2")
 }
